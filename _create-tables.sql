cae7be5aa30af9b34dc37dd9d75beaedb26e23e8
HoWC1999 <afletcher@hartford.edu>
2024-10-17 19:01:02 -0400

    Update _create-tables.sql
    Changed char limits

--- _create-tables.sql
+++ _create-tables.sql
@@ -7,13 +7,13 @@ SET @key_str = UNHEX(SHA2('SuperSecretKey123!', 256));
 -- Create the password_entries table to store the password data
 CREATE TABLE IF NOT EXISTS password_entries (
     id INT AUTO_INCREMENT PRIMARY KEY,             -- Auto-incrementing ID for unique identification of each entry
-    website_name VARCHAR(255) NOT NULL,            -- The name of the website (e.g., "Google", "LinkedIn")
-    website_url VARCHAR(255) NOT NULL,             -- The URL of the website (e.g., "https://google.com")
-    first_name VARCHAR(50) NOT NULL,               -- The first name of the user (e.g., "Andrew")
-    last_name VARCHAR(50) NOT NULL,                -- The last name of the user (e.g., "Fletcher")
-    username VARCHAR(50) NOT NULL,                 -- The username used for login on the site (e.g., "andrew.flet")
-    email VARCHAR(100) NOT NULL,                   -- The email address of the user (e.g., "afletcher@hartford.edu")
-    password VARBINARY(255) NOT NULL,              -- The encrypted password
+    website_name VARCHAR(512) NOT NULL,            -- The name of the website (e.g., "Google", "LinkedIn")
+    website_url VARCHAR(512) NOT NULL,             -- The URL of the website (e.g., "https://google.com")
+    first_name VARCHAR(64) NOT NULL,               -- The first name of the user (e.g., "Andrew")
+    last_name VARCHAR(64) NOT NULL,                -- The last name of the user (e.g., "Fletcher")
+    username VARCHAR(64) NOT NULL,                 -- The username used for login on the site (e.g., "andrew.flet")
+    email VARCHAR(128) NOT NULL,                   -- The email address of the user (e.g., "afletcher@hartford.edu")
+    password VARBINARY(512) NOT NULL,              -- The encrypted password
     comment TEXT,                                  -- Optional comment field for any additional notes
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Automatically records the time the entry was created
 );
